e045e58b44da61b0a59f500f54fd1a4777ec7179
Raja Ramzan <raja@shoptrader.nl>
2022-02-17 08:23:37 +0100

    added necessary packages for the .js implementation, but jquery is required. Need to find another way to implement.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Header from "./components/common/Header";
 import Navbar from "./components/common/Navbar";
 import Blog from "./components/Blog";
 import Footer from "./components/common/Footer";
+import Script from "./components/common/Script";
 
 
 function App() {
-  return (
-      <div>
-        <Header/>
-        <Navbar/>
-        <Blog/>
-        <Footer/>
-      </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Navbar/>
+            <Blog/>
+            <Footer/>
+            <Script/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/common/Script.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+// import '../../assets/js/theme.js';
+import * as bootstrap from 'bootstrap/dist/js/bootstrap.min';
+
+class Script extends Component {
+    render() {
+        return (
+            <div>
+                {bootstrap}
+            </div>
+        );
+    }
+}
+
+export default Script;
